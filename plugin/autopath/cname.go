e19b9972121f30368e126c062b4041657e7026b3
Stefan Budeanu <stefan.budeanu@shopify.com>
2019-03-15 04:34:29 -0400

    Fix corruption

--- plugin/autopath/cname.go
+++ plugin/autopath/cname.go
@@ -13,12 +13,11 @@ func cnamer(m *dns.Msg, original string) {
 		if strings.EqualFold(original, a.Header().Name) {
 			continue
 		}
-		m.Answer = append(m.Answer, nil)
-		copy(m.Answer[1:], m.Answer)
-		m.Answer[0] = &dns.CNAME{
+		cnameRecord := &dns.CNAME{
 			Hdr:    dns.RR_Header{Name: original, Rrtype: dns.TypeCNAME, Class: dns.ClassINET, Ttl: a.Header().Ttl},
 			Target: a.Header().Name,
 		}
+		m.Answer = append([]dns.RR{cnameRecord}, m.Answer...)
 		break
 	}
 	m.Question[0].Name = original
